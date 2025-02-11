0e442cc9fa0e03f99c279715dd0a4e3492dfa83d
unknown <jpcuadrelli@gmail.com>
2025-02-11 20:02:28 -0300

    test

--- src/app/api/index.ts
+++ src/app/api/index.ts
@@ -1,8 +1,9 @@
 import express from 'express';
+import cors from 'cors';
 
 const app = express();
 const port = 3000;
-
+app.use(cors());
 app.get('/api/test', (req, res) => {
     res.send('Endpoint de prueba funcionando correctamente');
 });
