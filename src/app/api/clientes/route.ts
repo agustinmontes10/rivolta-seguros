3061f745521bb5861eea8a2017e1a113aeb3c3fc
unknown <jpcuadrelli@gmail.com>
2025-02-13 20:12:33 -0300

    test

--- src/app/api/clientes/route.ts
+++ src/app/api/clientes/route.ts
@@ -1,8 +1,8 @@
 import { NextResponse } from "next/server";
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
-const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
+const supabaseUrl = process.env.SUPABASE_URL;
+const supabaseKey = process.env.SUPABASE_SERVICE_KEY;
 if (!supabaseUrl || !supabaseKey) {
   throw new Error("Supabase URL and Key must be provided.");
 }
